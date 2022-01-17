8d6484602b1ebed59a329db2fb8a3bbe3b73e3d5
Brad Westhafer <brad@yottadb.com>
2022-01-17 17:58:30 -0500

    Update README.md and add pre-commit hooks
    This commit updates `README.md` to include installation instructions and information about how to contribute.
    
    This commit also adds pre-commit hooks to the YDBEncrypt repo. These were copied from the YDBPosix repo to enforce copyright and remove trailing whitespace. The file `needs_copyright.sh` was also copied from the YDBPosix repo. It is used by the pre-commit hooks to keep copyrights up to date. In addition, this commit copies the `copyright.py` script from other repos that automatically updates copyrights and adjusts the pre-commit hooks to call `copyright.py` where appropriate.

--- README.md
+++ README.md
@@ -1,5 +1,55 @@
-# YDBencrypt
+# YDBEncrypt
 
-Provides an encryption reference plugin for YottaDB.
+YDBEncrypt provides an encryption reference plugin for YottaDB.
 
-This repository is a placeholder for when https://gitlab.com/YottaDB/DB/YDB/-/issues/306 is implemented. Until then, the encryption plugin code is part of YottaDB.
+# Installation
+
+Before installing YDBEncrypt, you will need to install the prerequisite packages
+
+Ubuntu or Debian Linux:
+```
+apt install -y git make gcc libgcrypt-dev libssl-dev libgpgme-dev libconfig-dev
+```
+
+Red Hat or Rocky Linux:
+```
+dnf install -y dnf-plugins-core
+dnf config-manager --set-enabled powertools
+dnf install -y git make gcc libgcrypt-devel openssl-devel gpgme-devel libconfig-devel
+```
+
+Since this is a plug-in for [YottaDB](https://gitlab.com/YottaDB/DB/YDB),
+YottaDB is required to use YDBEncrypt.
+
+YDBEncrypt can be installed while installing YottaDB by using the `--encplugin` command line option:
+
+```sh
+sudo ./ydbinstall.sh --encplugin
+```
+
+By default, `ydbinstall.sh` will build the current master branch of YottaDB.
+
+YDBEncrypt can also be installed after YottaDB is installed by running `ydbinstall.sh` using the `--encplugin` and `--plugins-only` command line options.
+
+```sh
+sudo ./ydbinstall.sh --plugins-only --encplugin
+```
+
+If you have multiple YottaDB versions installed, make sure the environment variable `$ydb_dist` is set to the correct version before running `ydbinstall.sh` with the `--plugins-only` command line option.
+
+To install YDBEncrypt without using `ydbinstall.sh`, you can build it from source with the following commands:
+
+```sh
+# Make sure that you have the ydb_dist environment variable defined in your shell before continuing
+make && sudo --preserve-env=ydb_dist make install
+```
+
+## Contributing
+
+To contribute or help with further development, [fork the repository](https://docs.gitlab.com/ee/gitlab-basics/fork-project.html), clone your fork to a local copy and begin contributing! Please also set up the pre-commit script to automatically enforce some coding conventions. Assuming you are in the top-level directory, the following will work:
+
+```sh
+ln -s ../../pre-commit .git/hooks
+```
+
+Note that this script requires `tcsh`.
--- needs_copyright.sh
+++ needs_copyright.sh
@@ -2,7 +2,7 @@
 
 #################################################################
 #								#
-# Copyright (c) 2021 YottaDB LLC and/or its subsidiaries.	#
+# Copyright (c) 2020-2022 YottaDB LLC and/or its subsidiaries.	#
 # All rights reserved.						#
 #								#
 #	This source code contains the intellectual property	#
@@ -12,6 +12,9 @@
 #								#
 #################################################################
 
+# Determines whether a file should need a copyright by its name
+# Returns 0 if it needs a copyright and 1 otherwise.
+# Returns 2 if an error occurs.
 set -eu
 
 if ! [ $# = 1 ]; then
@@ -21,28 +24,22 @@ fi
 
 file="$1"
 
-# Don't require deleted files to have a copyright
-if ! [ -e "$file" ]; then
-       exit 1
-fi
-
-skipextensions="ref png zwr html ci"	# List of extensions that cannot have copyrights.
+skipextensions="rst png html in"	# List of extensions that cannot have copyrights.
+	# .rst  -> file used to generate documentation. Since final documentation has
+	#		copyrights, this one does not require it.
 	# .png  -> these are images (i.e. binary files) used in the documentation.
 	#		Same reason as .rst for not requiring a copyright.
-	# .ref  -> reference files used by the test cases (e.g. tests/outref/T0001.ref).
-	#		Those have a fixed format and should not go through copyright changes.
-	# .zwr  -> zwrite format extract file (does not currently allow a comment character).
 	# .html -> there are a couple of files currently under doc/templates which don't need copyrights.
-	# .ci   -> e.g. calltab.ci stores the call-in table which does not currently have a provision for comment characters.
+	# .in   -> e.g. ydbposix.xc.in stores the external-call/call-out table which does not currently
+	#		have a provision for comment characters.
 if echo "$skipextensions" | grep -q -w "$(echo "$file" | awk -F . '{print $NF}')"; then
 	exit 1
 fi
 
-# Determines whether a file should need a copyright by its name
-# Returns 0 if it needs a copyright and 1 otherwise.
-skiplist="COPYING README.md LICENSE .gitignore"
+# Below is a list of specific files that do not have a copyright so ignore them
+skiplist="COPYING README.md"
 for skipfile in $skiplist; do
-	if [ "$file" = "$skipfile" ]; then
+	if [ $file = $skipfile ]; then
 		exit 1
 	fi
 done
