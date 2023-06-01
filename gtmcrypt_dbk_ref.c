YottaDB/YDBEncrypt

commit dcb3a607801ddca238a0fe50033b5dea13630ef6

[YottaDB/DB/YDB#780] [V70000] Merge GT.M V7.0-000 changes to encryption plugin into YDBEncrypt repo
* GT.M V7.0-000 in the YDB project changed the following files.
  - sr_unix/gtm_tls_impl.c
  - sr_unix/gtmcrypt_dbk_ref.c
  - sr_unix/gtmcrypt_dbk_ref.h
  - sr_unix/gtmcrypt_pk_ref.c
  - sr_unix/gtmcrypt_ref.c
  - sr_unix/gtmcrypt_util.c
  - sr_unix/gtmcrypt_util.h

* In YottaDB, these files have been moved to the YDBEncrypt repository/project. Therefore the above
  changes are incorporated to the following files in the YDBEncrypt project as part of this commit.
  - YDBEncrypt/gtm_tls_impl.c
  - YDBEncrypt/gtmcrypt_dbk_ref.c
  - YDBEncrypt/gtmcrypt_dbk_ref.h
  - YDBEncrypt/gtmcrypt_pk_ref.c
  - YDBEncrypt/gtmcrypt_ref.c
  - YDBEncrypt/gtmcrypt_util.c
  - YDBEncrypt/gtmcrypt_util.h

* While the incorporation was straightforward, one change was not incorporated as is and is noted below.
  In `sr_unix/gtm_tls_impl.c`, a `strncpy()` call was unnecessary as we determined the length of the
  string using a `strnlen()` call in the previous line. Therefore this was replaced with a `memcpy()`
  call in `YDBEncrypt/gtm_tls_impl.c`.

gtmcrypt_dbk_ref.c
@@ -1,9 +1,9 @@
 /****************************************************************
  *								*
- * Copyright (c) 2009-2019 Fidelity National Information	*
+ * Copyright (c) 2009-2021 Fidelity National Information	*
  * Services, Inc. and/or its subsidiaries. All rights reserved.	*
  *								*
- * Copyright (c) 2018-2020 YottaDB LLC and/or its subsidiaries.	*
+ * Copyright (c) 2018-2023 YottaDB LLC and/or its subsidiaries.	*
  * All rights reserved.						*
  *								*
  *	This source code contains the intellectual property	*
@@ -315,7 +315,7 @@ STATICFNDEF gtm_keystore_t *keystore_lookup_by_keyname_plus(char *keyname, char 
 	gtm_keystore_keyname_link_t 	*cur_node;
 	char				*ynew_ext;
 	char				lcl_keyname[YDB_PATH_MAX];
-	int				keynamelen;
+	unsigned int			keynamelen;
 
 	assert((SEARCH_BY_KEYPATH == search_type) || (SEARCH_BY_HASH == search_type));
 	assert(NULL != search_field);
@@ -324,11 +324,14 @@ STATICFNDEF gtm_keystore_t *keystore_lookup_by_keyname_plus(char *keyname, char 
 	keynamelen = strlen(keyname);
 	if (GTM_PATH_MAX < keynamelen)
 		keynamelen = GTM_PATH_MAX;
-	ynew_ext = keyname + keynamelen - STRLEN(EXT_NEW);
+	ynew_ext = keyname + keynamelen - strlen(EXT_NEW);
 	if ((ynew_ext >= keyname) && (0 == strcmp(ynew_ext, EXT_NEW)))
 	{	/* This is an autodb, fixup the path */
-		memcpy(lcl_keyname, keyname, keynamelen - STRLEN(EXT_NEW));
-		lcl_keyname[keynamelen - STRLEN(EXT_NEW)] = '\0';
+		assert(strlen(EXT_NEW) <= keynamelen);
+		cplen = (strlen(EXT_NEW) <= keynamelen) ? (keynamelen - strlen(EXT_NEW)) : 0;
+		assert(GTM_PATH_MAX > cplen);
+		memcpy(lcl_keyname, keyname, cplen);
+		lcl_keyname[cplen] = '\0';
 		keyname = lcl_keyname;
 	}
 	cur_node = keystore_by_keyname_head;
@@ -379,7 +382,7 @@ STATICFNDEF gtm_keystore_t *keystore_lookup_by_unres_key(char *search_field1, in
 	char				*name_search_field_ptr, *path_search_field_ptr, *ynew_ext;
 	char				*lcl_key_name, lcl_key_name_buff[YDB_PATH_MAX + SIZEOF(EXT_NEW) + 1];
 	char				name_search_field_buff[YDB_PATH_MAX];
-	int				search_field_len;
+	unsigned int			search_field_len, cplen;
 	int				isautodb;
 
 	assert(NULL != search_field1);
@@ -398,14 +401,17 @@ STATICFNDEF gtm_keystore_t *keystore_lookup_by_unres_key(char *search_field1, in
 			search_field_len = strlen(search_field1);
 			if (GTM_PATH_MAX < search_field_len)
 				search_field_len = GTM_PATH_MAX;
-			ynew_ext = search_field1 + search_field_len - STRLEN(EXT_NEW);
+			ynew_ext = search_field1 + search_field_len - strlen(EXT_NEW);
 			if (0 == strcmp(ynew_ext, EXT_NEW))
 			{	/* Strip EXT_NEW off the path string for comparison later. Note that this path, minus EXT_NEW,
 				 * is a fully resolved path.
 				 */
 				isautodb = TRUE;
-				memcpy(name_search_field_buff, search_field1, search_field_len - STRLEN(EXT_NEW));
-				name_search_field_buff[search_field_len - STRLEN(EXT_NEW)] = '\0';
+				assert(strlen(EXT_NEW) <= search_field_len);
+				cplen = (strlen(EXT_NEW) <= search_field_len) ? (search_field_len - strlen(EXT_NEW)) : 0;
+				assert(GTM_PATH_MAX > cplen);
+				memcpy(name_search_field_buff, search_field1, cplen);
+				name_search_field_buff[cplen] = '\0';
 				name_search_field_ptr = name_search_field_buff;
 			}
 		}
@@ -605,7 +611,8 @@ STATICFNDEF gtm_keystore_t *gtmcrypt_decrypt_key(char *key_path, int path_length
  */
 STATICFNDEF int keystore_refresh(void)
 {
-	int		n_mappings, status, just_read, envvar_len;
+	int		n_mappings, status, just_read;
+	size_t		envvar_len;
 	char		*config_env;
 	struct stat	stat_info;
 	boolean_t	is_ydb_env_match;
@@ -664,7 +671,9 @@ STATICFNDEF int keystore_refresh(void)
 			return -1;
 		}
 		/* The ydb_crypt_config variable is defined and accessible. Copy it to a global for future references. */
-		SNPRINTF(gc_config_filename, SIZEOF(gc_config_filename), "%s", config_env);
+		assert(envvar_len < SIZEOF(gc_config_filename));
+		memcpy(gc_config_filename, config_env, envvar_len);
+		gc_config_filename[envvar_len] = '\0';
 		just_read = TRUE;
 	}
 	assert(!CONFIG_FILE_UNREAD);
@@ -857,13 +866,14 @@ STATICFNDEF int read_database_section(config_t *cfgp)
  *
  * Returns:	0 if successfully created a new encryption / decryption state object; -1 otherwise.
  */
-int keystore_new_cipher_ctx(gtm_keystore_t *entry, char *iv, int length, int action)
+int keystore_new_cipher_ctx(gtm_keystore_t *entry, char *iv, unsigned int length, int action)
 {
 	int			rv;
 	crypt_key_t		handle = NULL;
 	gtm_cipher_ctx_t	*ctx;
 	unsigned char		iv_array[GTMCRYPT_IV_LEN];
 
+	assert(GTMCRYPT_IV_LEN >= length);	/* sanity check incoming length -- should never happen */
 	memset(iv_array, 0, GTMCRYPT_IV_LEN);
 	memcpy(iv_array, iv, length);
 	if (0 != (rv = gc_sym_create_cipher_handle(entry->key, iv_array, &handle, action, FALSE)))

gtm_tls_impl.c
@@ -1,9 +1,9 @@
 /****************************************************************
  *								*
- * Copyright (c) 2013-2020 Fidelity National Information	*
+ * Copyright (c) 2013-2021 Fidelity National Information	*
  * Services, Inc. and/or its subsidiaries. All rights reserved.	*
  *								*
- * Copyright (c) 2018-2022 YottaDB LLC and/or its subsidiaries.	*
+ * Copyright (c) 2018-2023 YottaDB LLC and/or its subsidiaries.	*
  * All rights reserved.						*
  *								*
  *	This source code contains the intellectual property	*
@@ -711,7 +711,7 @@ gtm_tls_ctx_t *gtm_tls_init(int version, int flags)
 			flags &= ~verify_level;
 		}
 		verify_level = (int)level_long;
-		flags = (~GTMTLS_OP_VERIFY_LEVEL_MASK & flags) | verify_level;
+		flags = (GTMTLS_OP_VERIFY_LEVEL_CMPLMNT & flags) | verify_level;
 	} else
 		flags |= GTMTLS_OP_VERIFY_LEVEL_DEFAULT;
 	rv1 = config_lookup_string(cfg, "tls.CAfile", &CAfile);
@@ -863,7 +863,7 @@ STATICFNDEF gtmtls_passwd_list_t *gtm_tls_find_pwent(ydbenvindx_t envindx, char 
 
 int gtm_tls_store_passwd(gtm_tls_ctx_t *tls_ctx, const char *tlsid, const char *obs_passwd)
 {
-	int			obs_len;
+	size_t			obs_len;
 	gtmtls_passwd_list_t	*pwent_node;
 	passwd_entry_t		*pwent;
 
@@ -878,11 +878,12 @@ int gtm_tls_store_passwd(gtm_tls_ctx_t *tls_ctx, const char *tlsid, const char *
 	if (NULL != pwent_node)
 	{
 		pwent = pwent_node->pwent;
-		if ((obs_len == (pwent->passwd_len * 2)) && (0 == strncmp(obs_passwd, pwent->passwd, obs_len)))
+		if ((obs_len == (size_t)(pwent->passwd_len * 2)) && (0 == strncmp(obs_passwd, pwent->passwd, obs_len)))
 			return 1;	/* already on the list */
 	}
 	/* Either no entry for tlsid or need to replace with new value */
 	pwent = MALLOC(SIZEOF(passwd_entry_t));
+	assert(NULL != pwent);
 	pwent->envindx = YDBENVINDX_TLS_PASSWD_PREFIX;
 	SNPRINTF(pwent->suffix, SIZEOF(pwent->suffix), "%s", tlsid);
 	pwent->env_value = MALLOC(obs_len + 1);
@@ -1175,9 +1176,9 @@ gtm_tls_socket_t *gtm_tls_socket(gtm_tls_ctx_t *tls_ctx, gtm_tls_socket_t *prev_
 	if (verify_mode_set)
 		SSL_set_verify(ssl, verify_mode, NULL);
 	if (verify_level_set)
-		flags = (~GTMTLS_OP_VERIFY_LEVEL_MASK & flags) | verify_level;
+		flags = (GTMTLS_OP_VERIFY_LEVEL_CMPLMNT & flags) | verify_level;
 	else
-		flags = (~GTMTLS_OP_VERIFY_LEVEL_MASK & flags) | (GTMTLS_OP_VERIFY_LEVEL_MASK & tls_ctx->flags);
+		flags = (GTMTLS_OP_VERIFY_LEVEL_CMPLMNT & flags) | (GTMTLS_OP_VERIFY_LEVEL_MASK & tls_ctx->flags);
 	if (NULL == cipher_list)
 	{	/* no cipher-list in labelled section or no section */
 		if (0 != ((GTMTLS_OP_ABSENT_CIPHER | GTMTLS_OP_DEFAULT_CIPHER) & tls_ctx->flags))
@@ -1386,9 +1387,8 @@ gtm_tls_socket_t *gtm_tls_socket(gtm_tls_ctx_t *tls_ctx, gtm_tls_socket_t *prev_
 		SNPRINTF(cfg_path, MAX_CONFIG_LOOKUP_PATHLEN, "tls.%s.session-id-hex", id);
 		if (CONFIG_TRUE == config_lookup_string(cfg, cfg_path, &session_id_hex))
 		{	/* convert hex to char and set len */
-			session_id_len = STRLEN(session_id_hex);
-			if (MAX_SESSION_ID_LEN < session_id_len)
-				session_id_len = MAX_SESSION_ID_LEN;	/* avoid overrun */
+			session_id_len = (int)strnlen(session_id_hex, (size_t)MAX_SESSION_ID_LEN);
+			assert((0 < session_id_len) && (MAX_SESSION_ID_LEN >= session_id_len));
 			GC_UNHEX(session_id_hex, session_id_string, session_id_len);
 			if (-1 == session_id_len)
 			{
@@ -1400,10 +1400,10 @@ gtm_tls_socket_t *gtm_tls_socket(gtm_tls_ctx_t *tls_ctx, gtm_tls_socket_t *prev_
 			session_id_len = session_id_len / 2;		/* bytes */
 		} else
 		{
-			session_id_len = STRLEN(id);
-			if (SIZEOF(session_id_string) <= session_id_len)
-				session_id_len = SIZEOF(session_id_string) - 1;
-			memcpy((char *)session_id_string, id, session_id_len);		/* default to tlsid */
+			assert(SSL_MAX_SSL_SESSION_ID_LENGTH == SIZEOF(session_id_string));
+			session_id_len = (int)strnlen(id, (size_t)(SSL_MAX_SSL_SESSION_ID_LENGTH - 1));
+			assert((0 < session_id_len) && (SSL_MAX_SSL_SESSION_ID_LENGTH > session_id_len));
+			memcpy((char *)session_id_string, id, (size_t)session_id_len);		/* default to tlsid */
 			session_id_string[session_id_len] = '\0';
 		}
 		if (0 >= SSL_set_session_id_context(ssl, (const unsigned char *)session_id_string, (unsigned int)session_id_len))
@@ -1472,6 +1472,10 @@ gtm_tls_socket_t *gtm_tls_socket(gtm_tls_ctx_t *tls_ctx, gtm_tls_socket_t *prev_
 	socket->flags = flags;
 	socket->ssl = ssl;
 	socket->gtm_ctx = tls_ctx;
+	assert((MAX_TLSID_LEN + 1) == SIZEOF(socket->tlsid));
+	session_id_len = (int)strnlen(id, (size_t)MAX_TLSID_LEN);
+	memcpy(socket->tlsid, (const char *)id, session_id_len);
+	socket->tlsid[session_id_len] = '\0';
 	SNPRINTF(socket->tlsid, SIZEOF(socket->tlsid), "%s", (const char *)id);
 	/* Now, store the `socket' structure in the `SSL' structure so that we can get it back in a callback that receives an
 	 * `SSL' structure. Ideally, we should be using SSL_set_ex_data/SSL_get_ex_data family of functions. But, these functions
@@ -1754,7 +1758,7 @@ int gtm_tls_renegotiate_options(gtm_tls_socket_t *socket, int msec_timeout, char
 		if (verify_mode_set)
 			SSL_set_verify(ssl, verify_mode, NULL);
 		if (verify_level_set)
-			flags = (~GTMTLS_OP_VERIFY_LEVEL_MASK & flags) | verify_level;
+			flags = (GTMTLS_OP_VERIFY_LEVEL_CMPLMNT & flags) | verify_level;
 		if ((NULL == CAfile) && !(GTMTLS_OP_CLIENT_CA & flags))
 		{	/* check for tlsid.CAfile or tls.CAfile */
 			SNPRINTF(cfg_path, MAX_CONFIG_LOOKUP_PATHLEN, "tls.%s.CAfile", socket->tlsid);

gtmcrypt_dbk_ref.h
@@ -1,9 +1,9 @@
 /****************************************************************
  *								*
- * Copyright (c) 2009-2016 Fidelity National Information	*
+ * Copyright (c) 2009-2021 Fidelity National Information	*
  * Services, Inc. and/or its subsidiaries. All rights reserved.	*
  *								*
- * Copyright (c) 2018 YottaDB LLC and/or its subsidiaries.	*
+ * Copyright (c) 2018-2023 YottaDB LLC and/or its subsidiaries.	*
  * All rights reserved.						*
  *								*
  *	This source code contains the intellectual property	*
@@ -173,7 +173,7 @@ STATICFNDEF gtm_keystore_t 	*keystore_lookup_by_keyname_plus(char *keyname, char
 STATICFNDEF gtm_keystore_t 	*keystore_lookup_by_keypath(char *keypath);
 STATICFNDEF gtm_keystore_t 	*keystore_lookup_by_unres_key(char *search_field1, int search_field1_type,
 				char *search_field2, int search_field2_type, int database, int *error);
-int 				keystore_new_cipher_ctx(gtm_keystore_t *entry, char *iv, int length, int action);
+int 				keystore_new_cipher_ctx(gtm_keystore_t *entry, char *iv, unsigned int length, int action);
 int				keystore_remove_cipher_ctx(gtm_cipher_ctx_t *ctx);
 STATICFNDEF void		print_debug(void);
 

gtmcrypt_pk_ref.c
@@ -1,9 +1,9 @@
 /****************************************************************
  *								*
- * Copyright (c) 2009-2019 Fidelity National Information	*
+ * Copyright (c) 2009-2021 Fidelity National Information	*
  * Services, Inc. and/or its subsidiaries. All rights reserved.	*
  *								*
- * Copyright (c) 2018-2020 YottaDB LLC and/or its subsidiaries.	*
+ * Copyright (c) 2018-2023 YottaDB LLC and/or its subsidiaries.	*
  * All rights reserved.						*
  *								*
  *	This source code contains the intellectual property	*
@@ -183,7 +183,8 @@ gpgme_error_t gc_pk_get_decrypted_key(const char *cipher_file, unsigned char *pl
 int gc_pk_gpghome_has_permissions()
 {
 	char		pathname[YDB_PATH_MAX], *ptr;
-	int		gnupghome_set, pathlen;
+	int		gnupghome_set;
+	size_t		pathlen;
 
 	/* See if GNUPGHOME is set in the environment */
 	if (!(ptr = ydb_getenv(YDBENVINDX_GENERIC_GNUPGHOME, NULL_SUFFIX, NULL_IS_YDB_ENV_MATCH)))

gtmcrypt_ref.c
@@ -1,9 +1,9 @@
 /****************************************************************
  *								*
- * Copyright (c) 2009-2016 Fidelity National Information	*
+ * Copyright (c) 2009-2021 Fidelity National Information	*
  * Services, Inc. and/or its subsidiaries. All rights reserved.	*
  *								*
- * Copyright (c) 2018-2019 YottaDB LLC and/or its subsidiaries.	*
+ * Copyright (c) 2018-2023 YottaDB LLC and/or its subsidiaries.	*
  * All rights reserved.						*
  *								*
  *	This source code contains the intellectual property	*
@@ -44,10 +44,10 @@
 
 #define CHECK_IV_LENGTH(IV)									\
 {												\
-	if (IV.length > GTMCRYPT_IV_LEN)							\
+	if ((GTMCRYPT_IV_LEN < IV.length) || (0 > IV.length))					\
 	{											\
 		UPDATE_ERROR_STRING("Specified IVEC has length %ld, which is greater than "	\
-			"the maximum allowed IVEC length %d", iv.length, GTMCRYPT_IV_LEN);	\
+			"the maximum allowed IVEC length %d", IV.length, GTMCRYPT_IV_LEN);	\
 		return -1;									\
 	}											\
 }
@@ -183,10 +183,10 @@ gtm_status_t gtmcrypt_init_db_cipher_context_by_hash(gtmcrypt_key_t *handle, gtm
 	assert(NULL != entry);
 	if (NULL == entry->db_cipher_entry)
 	{	/* This cipher context is for decryption; iv is a static global. */
-		if (0 != keystore_new_cipher_ctx(entry, iv.address, iv.length, GTMCRYPT_OP_DECRYPT))
+		if (0 != keystore_new_cipher_ctx(entry, iv.address, (unsigned int)iv.length, GTMCRYPT_OP_DECRYPT))
 			return -1;
 		/* And this cipher context (inserted ahead of the first one) is for encryption. */
-		if (0 != keystore_new_cipher_ctx(entry, iv.address, iv.length, GTMCRYPT_OP_ENCRYPT))
+		if (0 != keystore_new_cipher_ctx(entry, iv.address, (unsigned int)iv.length, GTMCRYPT_OP_ENCRYPT))
 			return -1;
 		entry->db_cipher_entry = entry->cipher_head;
 	} else
@@ -227,7 +227,7 @@ gtm_status_t gtmcrypt_init_device_cipher_context_by_keyname(gtmcrypt_key_t *hand
 	if (0 != gtmcrypt_getkey_by_keyname(keyname, NULL, &entry, FALSE))
 		return -1;
 	assert(NULL != entry);
-	if (0 != keystore_new_cipher_ctx(entry, iv.address, iv.length, operation))
+	if (0 != keystore_new_cipher_ctx(entry, iv.address, (unsigned int)iv.length, operation))
 		return -1;
 	ctx = (gtm_cipher_ctx_t **)handle;
 	*ctx = entry->cipher_head;

gtmcrypt_util.c
@@ -1,9 +1,9 @@
 /****************************************************************
  *								*
- * Copyright (c) 2013-2019 Fidelity National Information	*
+ * Copyright (c) 2013-2021 Fidelity National Information	*
  * Services, Inc. and/or its subsidiaries. All rights reserved.	*
  *								*
- * Copyright (c) 2018-2020 YottaDB LLC and/or its subsidiaries.	*
+ * Copyright (c) 2018-2023 YottaDB LLC and/or its subsidiaries.	*
  * All rights reserved.						*
  *								*
  *	This source code contains the intellectual property	*
@@ -147,6 +147,7 @@ int gc_read_passwd(char *prompt, char *buf, int maxlen, void *tty)
 			 * updating the passed-in pointer.
 			 */
 			tty_copy = (struct termios *)MALLOC(SIZEOF(struct termios));
+			assert(NULL != tty_copy);
 			memcpy(tty_copy, &old_tty, SIZEOF(struct termios));
 			*((struct termios **)tty) = tty_copy;
 		}
@@ -385,8 +386,10 @@ int gc_update_passwd(ydbenvindx_t envindx, char *suffix, passwd_entry_t **ppwent
 		if (NULL != pwent)
 			gc_freeup_pwent(pwent);
 		pwent = MALLOC(SIZEOF(passwd_entry_t));
+		assert(NULL != pwent);
 		pwent->envindx = envindx;
 		pwent->env_value = MALLOC(len ? len + 1 : GTM_PASSPHRASE_MAX * 2 + 1);
+		assert(NULL != pwent->env_value);
 		if (NULL != suffix)
 			strncpy(pwent->suffix, suffix, SIZEOF(pwent->suffix));
 		else
@@ -395,6 +398,7 @@ int gc_update_passwd(ydbenvindx_t envindx, char *suffix, passwd_entry_t **ppwent
 	}
 	pwent->passwd_len = len ? len / 2 + 1 : GTM_PASSPHRASE_MAX + 1;
 	pwent->passwd = MALLOC(pwent->passwd_len);
+	assert(NULL != pwent->passwd);
 	env_value = pwent->env_value;
 	passwd = pwent->passwd;
 	if (0 < len)

gtmcrypt_util.h
@@ -1,9 +1,9 @@
 /****************************************************************
  *								*
- * Copyright (c) 2013-2018 Fidelity National Information	*
+ * Copyright (c) 2013-2021 Fidelity National Information	*
  * Services, Inc. and/or its subsidiaries. All rights reserved.	*
  *								*
- * Copyright (c) 2018-2020 YottaDB LLC and/or its subsidiaries.	*
+ * Copyright (c) 2018-2023 YottaDB LLC and/or its subsidiaries.	*
  * All rights reserved.						*
  *								*
  *	This source code contains the intellectual property	*
@@ -94,7 +94,8 @@ MBSTART {											\
 		SNPRINTF(TARGET + i, LEN + 1 - i, "%02X", (unsigned char)SOURCE[i / 2]);	\
 } MBEND
 
-/* Note: "eintr_handling_check()" or "HANDLE_EINTR_OUTSIDE_SYSTEM_CALL" are not used (whether or not errno is EINTR) inside
+/* Using this macro implies that truncation errors are caught by some downstream check.
+ * Note: "eintr_handling_check()" or "HANDLE_EINTR_OUTSIDE_SYSTEM_CALL" are not used (whether or not errno is EINTR) inside
  * the encryption plugin code (gtmcrypt_util.h and gtmcrypt_util.c) because the plugin does not have access to the function code.
  * We do not expect the process to be indefinitely in the plugin (e.g. waiting for user input or IO etc.) and so it is
  * considered okay not to do this check for now while still inside the plugin.

commit 2bbfb9eea94c1ce50086819b33328ce4b27e2766

[YottaDB/DB/YDB#780] [V70000] Fix merge issue in gtmcrypt_dbk_ref.c (regression in dcb3a6078)
* While merging the GT.M V7.0-000 changes to sr_unix/gtmcrypt_dbk_ref.c (in dcb3a6078), the
  declaration of a local variable `cplen` was missed out. This caused a build failure when
  using the YDBEncrypt master branch for building YottaDB. This is fixed in the current commit.

gtmcrypt_dbk_ref.c
@@ -315,7 +315,7 @@ STATICFNDEF gtm_keystore_t *keystore_lookup_by_keyname_plus(char *keyname, char 
 	gtm_keystore_keyname_link_t 	*cur_node;
 	char				*ynew_ext;
 	char				lcl_keyname[YDB_PATH_MAX];
-	unsigned int			keynamelen;
+	unsigned int			keynamelen, cplen;
 
 	assert((SEARCH_BY_KEYPATH == search_type) || (SEARCH_BY_HASH == search_type));
 	assert(NULL != search_field);